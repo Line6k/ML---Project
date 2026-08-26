c86fecf0bf77cb0ea65bde1e9fe977c4a9f6f669
Line6k <kirper2004@gmail.com>
2026-08-26 13:52:01 +0300

    Imported Random forest model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -68,7 +68,7 @@ learner = ClassificationAlgorithms()
 max_features = 10
 
 selected_features, ordered_features, ordered_scores = learner.forward_selection(
-    max_features, x_train, y_train
+    max_features, X_train, y_train
 )
 
 selected_features = [
@@ -337,6 +337,47 @@ plt.ylabel("True label")
 plt.xlabel("Predicted label")
 plt.grid(False)
 plt.show()
+
+# 2
+
+
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y,
+) = learner.random_forest(
+    X_train[selected_features], y_train, X_test[feature_set_4], gridsearch=False
+)
+
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
+
 
 # --------------------------------------------------------------
 # Try a simpler model with the selected features
